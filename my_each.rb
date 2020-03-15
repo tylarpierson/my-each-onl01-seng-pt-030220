7f8917382242fb88b369d932ad6b611731a793a2
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 20:15:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,13 @@ def my_each(array)
   if block_given?
     i = 0 
     while i < array.length 
-      yield.array[i]
+      yield array[i]
       i = i + 1 
     end
     array
   end
 end
 
+my_each(words) do |word|
+  puts word 
+end
